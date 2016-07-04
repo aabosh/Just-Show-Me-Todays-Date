aabb7455a029dd5ea4a4115076af423da27bdda2
Andrew Abosh <andrewaboshartworks@gmail.com>
2016-07-04 00:38:52 -0400

    Initial Commit

---
+++ Just Show Me Todays Date/Just Show Me Todays Date/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Just Show Me Today's Date
+//
+//  Created by Andrew Abosh on 2016-07-03.
+//  Copyright © 2016 Andrew Abosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    let date = DateFormatter.localizedString(from: NSDate() as Date, dateStyle: .fullStyle, timeStyle: .noStyle)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dateLabel.text = date
+        
+    }
+
+}
---
+++ Just Show Me Todays Date/Today's Date/TodayViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TodayViewController.swift
+//  Today's Date
+//
+//  Created by Andrew Abosh on 2016-07-03.
+//  Copyright © 2016 Andrew Abosh. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    let date = DateFormatter.localizedString(from: NSDate() as Date, dateStyle: .fullStyle, timeStyle: .noStyle)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: ((NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        dateLabel.text = date
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
